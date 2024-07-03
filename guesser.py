67ac52915226e9071699984e84792bb563a0b43e
aarush569 <kanigiritrivedaarush@gmail.com>
2024-07-03 18:22:32 +0530

    Add files via upload

---
+++ guesser.py
@@ -0,0 +1,13 @@
+import random
+
+"""a=random.random()
+print(a)"""
+b=random.randint(1,6)
+
+num=int(input("Enter the number :"))
+if num==b:
+    print("Yeah! You guessed it right!!")
+else:
+    print("Better luck next time")
+
+print(b)
